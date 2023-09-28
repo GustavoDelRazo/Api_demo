32d7506a6829996b378c2515fcc05b143ff77318
Gustavo Del Razo <1722110326@utectulancingo.edu.mx>
2023-09-28 16:53:59 +0000

    Commit

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-
 from fastapi import FastAPI
+import csv
+import json
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message":"Hello World"}
+    return {"message": "Hello World"}
 
 @app.get("/v1/contactos")
 async def get_contactos():
-    # TODO read contactos.csv
-    # TODO JSON encode contactos.csv
-    # TODO save in response
-    response = []
+    # Inicializar una lista para almacenar los datos de los contactos
+    contactos = []
+
+    try:
+        # Abrir el archivo CSV y leer los datos
+        with open("contactos.csv", newline="") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                contactos.append(row)
+    except FileNotFoundError:
+        return {"error": "El archivo de contactos no fue encontrado"}
+
+    # Codificar los datos de contacto en formato JSON
+    contactos_json = json.dumps(contactos)
+
+    # Crear la respuesta con los datos codificados en JSON
+    response = {"contactos": contactos_json}
     return response
